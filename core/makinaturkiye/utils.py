579951a06456dbbcdc01b916db01ff9ed7130741
alien <eng@zaki.com>
2023-02-24 03:11:38 +0200

    test

--- core/makinaturkiye/utils.py
+++ core/makinaturkiye/utils.py
@@ -13,9 +13,13 @@ def remove_image_watermark(image_url):
     # print(f"Original size : {image.size}")  # 5464x3640
     image = image.resize((500, 375))
     name = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=35))
-    path = f'media/temp/images/{name}.jpg'
-    image.save(path)
-    return f'/{path}'
+    # path = f'media/temp/images/{name}.jpg'
+    directory_path = os.path.join('/media', 'images')
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+    file_path = os.path.join(directory_path, f'{name}.jpg')
+    image.save(file_path)
+    return f'/{file_path}'
 
     # with open(path, 'wb') as f:
     #     f.write(r.content)
