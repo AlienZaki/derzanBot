10db8a23484c488d71e36b09589d531baf7283c6
alien <eng@zaki.com>
2023-03-03 20:20:42 +0200

    updated category

--- core/models.py
+++ core/models.py
@@ -10,7 +10,7 @@ from django.db import models
 #         abstract = True
 
 class Product(models.Model):
-    code = models.CharField(max_length=50, unique=True, primary_key=True)
+    code = models.CharField(max_length=50, unique=True, primary_key=True, db_index=True)
     url = models.URLField()
     vendor = models.CharField(max_length=255)
     language = models.CharField(max_length=100)
@@ -22,7 +22,7 @@ class Product(models.Model):
     origin = models.CharField(max_length=255, null=True, blank=True)
     delivery_status = models.CharField(max_length=255, null=True, blank=True)
     guarantee = models.CharField(max_length=255, null=True, blank=True)
-    currency = models.CharField(max_length=150, null=True, blank=True)
+    currency = models.CharField(max_length=150, null=True, blank=True, db_index=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     price_description = models.CharField(max_length=255, null=True, blank=True)
     phone = models.CharField(max_length=20, null=True, blank=True)
@@ -47,7 +47,7 @@ class ProductImage(models.Model):
 
 class Task(models.Model):
     product_url = models.URLField(unique=True)
-    status = models.BooleanField(default=False)
+    status = models.BooleanField(default=False, db_index=True)
 
     def __str__(self):
         return f'{self.product_url} - {self.status}'
